65f20ba49fdcfb51e90d05ae151f328fc7ea285d
oktntko <oktntko@gmail.com>
2020-12-01 13:05:01 +0900

    temp

--- scripts/git/log.sh
+++ scripts/git/log.sh
@@ -3,23 +3,78 @@
 # git log [<options>] [<revision range>] [[--] <path>…​]
 
 fit::log::fzf() {
-  local preview_window_hidden
+  local -a options branches pathes
+  options=()
+  branches=()
+  pathes=()
+
+  local all_branches tags remotes is_path
 
   for x in "$@"; do
-    if [[ ${x} =~ -.* ]]; then
-      # オプションがあったらプレビューは非表示
-      preview_window_hidden="--preview-window=:hidden"
-      break
+    if [[ ${x} == -- ]]; then
+      is_path=true # [--]区切り文字 以降はすべて path
+    elif [[ ${x} == --oneline || ${x} == --decorate || ${x} == --graph ]]; then
+      : # --oneline --decorate --graph は無視
+
+    elif [[ ${x} == --branches ]]; then
+      all_branches="--branches" # branchesは特別扱い
+
+    elif [[ ${x} == --tags ]]; then
+      tags="--tags" # tagsは特別扱い
+
+    elif [[ ${x} == --remotes ]]; then
+      remotes="--remotes" # remotesは特別扱い
+
+    elif [[ ${x} =~ -.* ]]; then
+      # options
+      options=("${options[*]}" "${x}")
+
+      # TODO: オプションの選別
+
+    else
+      # commit or path
+      if [[ "${is_path}" || -f ${x} ]]; then
+        pathes=("${pathes[*]}" "${x}")
+        is_path=true
+      else
+        branches=("${branches[*]}" "${x}")
+      fi
     fi
   done
 
   local header
-  header="🔹KeyBindings🔹
-  ${BLUE}${S_UNDERLINE}ENTER${NORMAL} ${CYAN}❯ git show${NORMAL}
+  header="🔹KeyBindings
+  ${CYAN}${S_UNDERLINE}ENTER${NORMAL}  ${WHITE}❯ git show${NORMAL}
+  ${CYAN}ctrl+F${NORMAL} ${WHITE}❯ git difftool${NORMAL} (multiselect)
+  ${CYAN}ctrl+D${NORMAL} ${WHITE}❯ ${GREEN}fit${WHITE} diff${NORMAL} (multiselect)
 
 "
-  # オプションがあったら header も非表示。 普通に git log | fzf したときと同じ
-  [[ -n ${preview_window_hidden} ]] && header=""
+
+  local preview_window_hidden
+  if [[ ${#options[*]} -gt 0 ]]; then
+    # オプションがあったらプレビューは非表示
+    preview_window_hidden="--preview-window=:hidden"
+    # オプションがあったら header も非表示。 普通に git log | fzf したときと同じ
+    header=""
+  fi
+
+  # コマンドを生成
+  local git_log
+  if [[ ${#options[*]} -gt 0 ]]; then
+    git_log="fit git log \"$*\""
+  else
+    git_log=$(
+      echo "git log \\
+        --graph \\
+        --color=always \\
+        --pretty=\"[%C(yellow)%h%Creset]%C(auto)%d%Creset %s %C(dim)%an%Creset (%C(blue)%ad%Creset)\" \\
+        --date=format:\"%Y-%m-%d\" \\
+        \"${all_branches}\" \\
+        \"${tags}\" \\
+        \"${remotes}\" \\
+        $([[ ${#pathes[*]} -gt 0 ]] && echo "--") ${pathes[*]}" | sed -e 's/\n/ /g' | sed -e 's/ \+/ /g'
+    )
+  fi
 
   local fit_fzf
   fit_fzf="fit::fzf \\
@@ -27,11 +82,12 @@ fit::log::fzf() {
     --multi \\
     --preview \"fit log::preview {}\" \\
     --bind \"ctrl-d:execute(fit log::actions::call-diff {+})\" \\
+    --bind \"ctrl-f:execute(fit log::actions::call-difftool {+})\" \\
     --bind \"enter:execute(fit log::actions::call-show {} | eval ${FIT_PAGER_SHOW} | less -R)\" \\
     ${preview_window_hidden}
 "
 
-  if [[ -n ${preview_window_hidden} ]]; then fit::git log "$@"; else _fit::log::format "$@"; fi | eval "$fit_fzf"
+  eval "${git_log}" | sed -e '$d' | eval "$fit_fzf"
 
   [[ -z ${preview_window_hidden} ]] && _fit::log::format "$@" -10 && return
 }
@@ -55,18 +111,7 @@ fit::log::actions::call-diff() {
   extracts=$(_fit::log::extract "$@" | awk -v 'ORS= ' '{print $1}')
   [[ -z ${extracts} ]] && return
 
-  local -A commits
-  commits=(
-    ["old"]=""
-    ["new"]=""
-  )
-
-  for x in ${extracts}; do
-    commits["old"]="${commits["new"]}"
-    commits["new"]="${x}"
-  done
-
-  fit::diff "${commits[*]}"
+  fit::diff "${extracts[*]}"
 }
 
 fit::log::actions::call-show() {
