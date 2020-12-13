a7493f61ef9d50bd65f85077cf92c260003624ca
oktntko <oktntko@gmail.com>
2020-12-14 01:05:53 +0900

    git stash

---
+++ scripts/git/stash.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+fit::stash::fzf() {
+  local header
+  header="${GRAY}*${NORMAL} ${WHITE}KeyBindings${NORMAL}                           ${GRAY}*${NORMAL} ${WHITE}Change Options${NORMAL}
+| ${WHITE}${S_UNDERLINE}ENTER${NORMAL}  ${WHITE}❯${NORMAL} ${GREEN}git${NORMAL} show                     | Ctrl+${WHITE}B${NORMAL} ❯ ${GREEN}fit${NORMAL} log --branches
+| Ctrl+${WHITE}F${NORMAL} ${WHITE}❯${NORMAL} ${GREEN}git${NORMAL} difftool (multiselect)   | Ctrl+${WHITE}R${NORMAL} ${WHITE}❯${NORMAL} ${GREEN}fit${NORMAL} log --remotes
+| Ctrl+${WHITE}D${NORMAL} ${WHITE}❯${NORMAL} ${GREEN}fit${NORMAL} diff (multiselect)       | Ctrl+${WHITE}A${NORMAL} ${WHITE}❯${NORMAL} ${GREEN}fit${NORMAL} log --all
+
+"
+  local fit_fzf
+  fit_fzf="fit::fzf \\
+        --header \"$header\" \\
+        "
+
+  fit::stash::menu | eval "${fit_fzf}"
+}
+
+fit::stash::menu() {
+  echo "save"
+  echo "list"
+  echo "apply"
+  echo "pop"
+  echo "drop"
+  echo "clear"
+}
