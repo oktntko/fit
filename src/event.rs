5dd1ea53cb2c58e58e78bc4c687e7f7b3568e52d
oktntko <oktntko@gmail.com>
2021-01-22 15:22:03 +0900

    remove demo code

--- src/event.rs
+++ src/event.rs
@@ -1,12 +1,8 @@
+use log::{debug, error};
 use std::io;
 use std::sync::mpsc;
-use std::sync::{
-  atomic::{AtomicBool, Ordering},
-  Arc,
-};
 use std::thread;
 use std::time::Duration;
-
 use termion::event::Key;
 use termion::input::TermRead;
 
@@ -15,67 +11,60 @@ pub enum Event<I> {
   Tick,
 }
 
-/// A small event handler that wrap termion input and tick events. Each event
-/// type is handled in its own thread and returned to a common `Receiver`
-pub struct Events {
-  rx: mpsc::Receiver<Event<Key>>,
-  input_handle: thread::JoinHandle<()>,
-  ignore_exit_key: Arc<AtomicBool>,
-  tick_handle: thread::JoinHandle<()>,
-}
-
 #[derive(Debug, Clone, Copy)]
 pub struct Config {
-  pub exit_key: Key,
   pub tick_rate: Duration,
 }
 
 impl Default for Config {
   fn default() -> Config {
     Config {
-      exit_key: Key::Char('q'),
       tick_rate: Duration::from_millis(250),
     }
   }
 }
+/// A small event handler that wrap termion input and tick events. Each event
+/// type is handled in its own thread and returned to a common `Receiver`
+#[allow(dead_code)]
+pub struct Events {
+  rx: mpsc::Receiver<Event<Key>>,
+  input_handle: thread::JoinHandle<()>,
+  tick_handle: thread::JoinHandle<()>,
+}
 
 impl Events {
-  pub fn new() -> Events {
-    Events::with_config(Config::default())
-  }
-
-  pub fn with_config(config: Config) -> Events {
+  pub fn new(config: Config) -> Events {
+    debug!("config {:?}", config);
+    // Senderと Receiverを作る
     let (tx, rx) = mpsc::channel();
-    let ignore_exit_key = Arc::new(AtomicBool::new(false));
+    // input handle
     let input_handle = {
       let tx = tx.clone();
-      let ignore_exit_key = ignore_exit_key.clone();
       thread::spawn(move || {
         let stdin = io::stdin();
         for evt in stdin.keys() {
           if let Ok(key) = evt {
             if let Err(err) = tx.send(Event::Input(key)) {
-              eprintln!("{}", err);
-              return;
-            }
-            if !ignore_exit_key.load(Ordering::Relaxed) && key == config.exit_key {
+              error!("error! {}", err);
               return;
             }
           }
         }
       })
     };
+    // tick handle
     let tick_handle = {
       thread::spawn(move || loop {
         if tx.send(Event::Tick).is_err() {
+          error!("error!");
           break;
         }
         thread::sleep(config.tick_rate);
       })
     };
+    // return
     Events {
       rx,
-      ignore_exit_key,
       input_handle,
       tick_handle,
     }
@@ -83,13 +72,5 @@ impl Events {
 
   pub fn next(&self) -> Result<Event<Key>, mpsc::RecvError> {
     self.rx.recv()
-  }
-
-  pub fn disable_exit_key(&mut self) {
-    self.ignore_exit_key.store(true, Ordering::Relaxed);
-  }
-
-  pub fn enable_exit_key(&mut self) {
-    self.ignore_exit_key.store(false, Ordering::Relaxed);
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-use log::{debug, error, info, trace, warn};
+use log::debug;
 use log4rs;
-mod demo;
-#[allow(dead_code)]
-mod util;
+mod event;
 
-use crate::{
-  demo::{ui, App},
-  util::event::{Config, Event, Events},
-};
 use argh::FromArgs;
-use std::{error::Error, io, time::Duration};
+use event::{Config, Event, Events};
+use std::{error::Error, io};
 use termion::{event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
-use tui::{backend::TermionBackend, Terminal};
+use tui::{
+  backend::TermionBackend,
+  widgets::{Block, Borders},
+  Terminal,
+};
 
 /// Termion demo
 #[derive(Debug, FromArgs)]
@@ -25,50 +24,52 @@ struct Cli {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+  // -- init start -- //
+  // init log
   log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
 
   let cli: Cli = argh::from_env();
 
-  let events = Events::with_config(Config {
-    tick_rate: Duration::from_millis(cli.tick_rate),
-    ..Config::default()
-  });
-
+  // raw mode
   let stdout = io::stdout().into_raw_mode()?;
+  // termion <- stdout
   let stdout = MouseTerminal::from(stdout);
   let stdout = AlternateScreen::from(stdout);
   let backend = TermionBackend::new(stdout);
+  // tui <- termion
   let mut terminal = Terminal::new(backend)?;
 
-  let mut app = App::new("Termion demo", cli.enhanced_graphics);
+  // event reciever & sender
+  let events = Events::new(Config::default());
+
+  // -- init end -- //
+
   loop {
-    terminal.draw(|f| ui::draw(f, &mut app))?;
+    terminal.draw(|f| {
+      debug!("terminal.draw");
+      let size = f.size();
+      let block = Block::default().title("Block").borders(Borders::ALL);
+      f.render_widget(block, size);
+    });
 
     match events.next()? {
       Event::Input(key) => match key {
-        Key::Char(c) => {
-          app.on_key(c);
-        }
-        Key::Up => {
-          app.on_up();
-        }
-        Key::Down => {
-          app.on_down();
-        }
-        Key::Left => {
-          app.on_left();
-        }
-        Key::Right => {
-          app.on_right();
-        }
+        Key::Char(c) => match c {
+          'q' => {
+            break;
+          }
+          't' => {
+            break;
+          }
+          _ => {}
+        },
+        Key::Up => {}
+        Key::Down => {}
+        Key::Left => {}
+        Key::Right => {}
         _ => {}
       },
-      Event::Tick => {
-        app.on_tick();
-      }
-    }
-    if app.should_quit {
-      break;
+      Event::Tick => {}
     }
   }
 
